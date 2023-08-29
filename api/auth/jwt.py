2c0fdb80578cc10371983d07f549ea8357d6a630
alex <alex@email.com>
2023-08-29 15:23:35 -0300

    criada a função de autenticação do user e a criação do token para autorização de acesso a partes da aplicação

--- api/auth/jwt.py
+++ api/auth/jwt.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Any
 from services.user_service import UserService
@@ -8,6 +8,12 @@ auth_router = APIRouter()
 @auth_router.post('/login')
 async def login(data: OAuth2PasswordRequestForm = Depends()) -> Any:
     usuario = await UserService.authenticate(
-        email=data.email,
+        email=data.username,
         password=data.password
     )
+    if not usuario:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='E-mail ou Senha estão incorretos'
+        )
+    # Criar access_token
